82f82fb125579dae2329d999a599a705fca9339f
Fernanda <fernandaromero85@gmail.com>
2020-02-14 12:04:07 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-Practice-Emojipedia
-Created with CodeSandbox
---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function newEntry(detail) {
+  return (
+    <Entry
+      key={detail.id}
+      emoji={detail.emoji}
+      name={detail.name}
+      meaning={detail.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(newEntry)}</dl>
+    </div>
+  );
+}
+
+export default App;
